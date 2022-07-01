3c2a8120909c2246399e18fbee50008387051c95
waqas ahmad <waqaskanju@gmail.com>
2022-07-01 12:56:47 -0700

    correct the repair symbol

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,21 +2,19 @@ import React from 'react';
 import calculate from '../logic/calculate';
 import '../index.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = React.useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  btnClick = (e) => {
+  const btnClick = (e) => {
     const { value } = e.target;
-    const { total, next, operation } = calculate(this.state, value);
-    this.setState({ total, next, operation });
+    const { total, next, operation } = calculate(state, value);
+    setState({ total, next, operation });
     const result = document.getElementById('calcDisplay');
+
     if (next !== null) {
       result.innerHTML = next;
     } else if (total !== null) {
@@ -26,42 +24,39 @@ class Calculator extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <div className="calculator">
-        <div className="calc-display" id="calcDisplay"><span>0</span></div>
-        <div className="top-options">
-          <button type="button" onClick={this.btnClick} value="AC">AC</button>
-          <button type="button" onClick={this.btnClick} value="x/-">+/-</button>
-          <button type="button" onClick={this.btnClick}>%</button>
-          <button type="button" onClick={this.btnClick} value="+" className="orange">+</button>
-        </div>
-        <div className="second-row">
-          <button type="button" onClick={this.btnClick} value="7">7 </button>
-          <button type="button" onClick={this.btnClick} value="8">8</button>
-          <button type="button" onClick={this.btnClick} value="9">9</button>
-          <button type="button" onClick={this.btnClick} value="*" className="orange">*</button>
-        </div>
-        <div className="third-row">
-          <button type="button" onClick={this.btnClick} value="4">4 </button>
-          <button type="button" onClick={this.btnClick} value="5">5</button>
-          <button type="button" onClick={this.btnClick} value="6">6</button>
-          <button type="button" onClick={this.btnClick} value="-" className="orange">-</button>
-        </div>
-        <div className="first-row">
-          <button type="button" onClick={this.btnClick} value="1">1 </button>
-          <button type="button" onClick={this.btnClick} value="2">2</button>
-          <button type="button" onClick={this.btnClick} value="3">3</button>
-          <button type="button" onClick={this.btnClick} value="+" className="orange">+</button>
-        </div>
-        <div className="bottom-row flex">
-          <button type="button" onClick={this.btnClick} value="0" className="flex-child">0 </button>
-          <button type="button" onClick={this.btnClick} value=".">.</button>
-          <button type="button" onClick={this.btnClick} value="=" className="orange">=</button>
-        </div>
+  return (
+    <div className="calculator">
+      <div className="calc-display" id="calcDisplay"><span>0</span></div>
+      <div className="top-options">
+        <button type="button" onClick={btnClick} value="AC">AC</button>
+        <button type="button" onClick={btnClick} value="x/-">+/-</button>
+        <button type="button" onClick={btnClick}>%</button>
+        <button type="button" onClick={btnClick} value="/" className="orange">/</button>
       </div>
-    );
-  }
-}
-
+      <div className="second-row">
+        <button type="button" onClick={btnClick} value="7">7 </button>
+        <button type="button" onClick={btnClick} value="8">8</button>
+        <button type="button" onClick={btnClick} value="9">9</button>
+        <button type="button" onClick={btnClick} value="*" className="orange">*</button>
+      </div>
+      <div className="third-row">
+        <button type="button" onClick={btnClick} value="4">4 </button>
+        <button type="button" onClick={btnClick} value="5">5</button>
+        <button type="button" onClick={btnClick} value="6">6</button>
+        <button type="button" onClick={btnClick} value="-" className="orange">-</button>
+      </div>
+      <div className="first-row">
+        <button type="button" onClick={btnClick} value="1">1 </button>
+        <button type="button" onClick={btnClick} value="2">2</button>
+        <button type="button" onClick={btnClick} value="3">3</button>
+        <button type="button" onClick={btnClick} value="+" className="orange">+</button>
+      </div>
+      <div className="bottom-row flex">
+        <button type="button" onClick={btnClick} value="0" className="flex-child">0 </button>
+        <button type="button" onClick={btnClick} value=".">.</button>
+        <button type="button" onClick={btnClick} value="=" className="orange">=</button>
+      </div>
+    </div>
+  );
+};
 export default Calculator;
