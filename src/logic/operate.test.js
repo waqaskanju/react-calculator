5ff9ea2b53a4c0920753fc7f0dcc24271f4508b0
waqas ahmad <waqaskanju@gmail.com>
2022-07-06 15:21:38 -0700

    test for minus added

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -3,3 +3,7 @@ import operate from './operate';
 test('adds 7 + 8 to equal 15', () => {
   expect(operate(7, 8, '+')).toBe('15');
 });
+
+test('adds 20 - 7 to equal 13', () => {
+  expect(operate(20, 7, '-')).toBe('13');
+});
