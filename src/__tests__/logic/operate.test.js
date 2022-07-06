5e0ad236366a0dcaa6e7833f3297858449c1bd40
worashf <uppert83@gmail.com>
2022-07-07 01:45:39 +0300

    add operate function tests

---
+++ src/__tests__/logic/operate.test.js
@@ -0,0 +1,22 @@
+import operate from '../../logic/operate';
+
+describe('test our operate function', () => {
+  test('adds 7 + 8 to equal 15', () => {
+    expect(operate(7, 8, '+')).toBe('15');
+  });
+
+  test('adds 20 - 7 to equal 13', () => {
+    expect(operate(20, 7, '-')).toBe('13');
+  });
+  test('divide 10 by 2  equal to 5', () => {
+    const result = operate(10, 2, '÷');
+    expect(result).toBe('5');
+  });
+  test('divide 10 by 0   should be Can not divide by zero', () => {
+    const result = operate(10, 0, '÷');
+    expect(result).toContain("Can't divide by 0.");
+  });
+  test('multipy 10 by 5 should be 50', () => {
+    expect(operate(10, 5, '*')).toBe('50');
+  });
+});
